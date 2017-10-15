ab04ecdeb45f2b86d82f26702890ca965c8550aa
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-10-15 13:48:01 +0100

    Placeholder

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="regent",
+    version="0.1",
+    author="Matt Molyneaux",
+    author_email="moggers87+git@moggers87.co.uk",
+    packages=["regent"],
+)
