f419d70b1296793a10483862ef56f1b6b90fc88e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-06-15 22:44:30 +0100

    regent → exhibition
    Because regent was a stupid name anyways

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="regent",
+    name="exhibition",
     version="0.1",
     author="Matt Molyneaux",
     author_email="moggers87+git@moggers87.co.uk",
-    packages=["regent"],
+    packages=["exhibition"],
 )
