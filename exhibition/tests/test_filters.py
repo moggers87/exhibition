f224ee404a30d1762a6564b901b45ed7b9001cc3
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-06-23 22:47:44 +0100

    Test stubs for filters and nodes

---
+++ exhibition/tests/test_filters.py
@@ -0,0 +1,43 @@
+##
+#
+# Copyright (C) 2018 Matt Molyneaux
+#
+# This file is part of Exhibition.
+#
+# Exhibition is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Exhibition is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Exhibition.  If not, see <https://www.gnu.org/licenses/>.
+#
+##
+
+from unittest import TestCase
+
+
+class Jinja2TestCase(TestCase):
+    def test_template(self):
+        # no extends or anything
+        pass
+
+    def test_extends(self):
+        pass
+
+    def test_extends_with_multiple_template_dirs(self):
+        pass
+
+    def test_default_block(self):
+        pass
+
+    def test_context(self):
+        pass
+
+    def test_raise_extension(self):
+        pass
---
+++ exhibition/tests/test_node.py
@@ -0,0 +1,69 @@
+##
+#
+# Copyright (C) 2018 Matt Molyneaux
+#
+# This file is part of Exhibition.
+#
+# Exhibition is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Exhibition is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Exhibition.  If not, see <https://www.gnu.org/licenses/>.
+#
+##
+
+from unittest import TestCase
+
+
+class NodeTestCase(TestCase):
+    def test_repr(self):
+        pass
+
+    def test_full_path(self):
+        pass
+
+    def test_full_url(self):
+        pass
+
+    def test_walk(self):
+        pass
+
+    def test_render_dir(self):
+        pass
+
+    def test_render_file(self):
+        pass
+
+    def test_process_good_meta(self):
+        pass
+
+    def test_process_bad_meta(self):
+        pass
+
+    def test_process_no_meta(self):
+        pass
+
+    def test_get_content(self):
+        pass
+
+    def test_data(self):
+        pass
+
+    def test_add_child(self):
+        pass
+
+    def test_siblings(self):
+        pass
+
+    def test_from_path(self):
+        pass
+
+    def test_from_path_and_ignore(self):
+        pass
