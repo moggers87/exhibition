eb2b073c182e92c1280f250d5ff3a0bc174e0d31
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2018-07-24 14:14:45 +0100

    Tests for filter globbing

--- exhibition/tests/test_filters.py
+++ exhibition/tests/test_filters.py
@@ -246,3 +246,52 @@ class Jinja2TestCase(TestCase):
 
         # dict key order isn't stable, but it'll be one of these
         self.assertIn(result, ["FalseNone", "NoneFalse"])
+
+    def test_filter(self):
+        with TemporaryDirectory() as content_path, TemporaryDirectory() as deploy_path:
+            path = pathlib.Path(content_path, "blog.html")
+            with path.open("w") as f:
+                f.write(PLAIN_TEMPLATE)
+
+            node = Node(path, Node(path.parent, None, {"content_path": content_path,
+                                                       "deploy_path": deploy_path,
+                                                       "filter": "exhibition.filters.jinja2",
+                                                       "templates": []}))
+            node.render()
+            with pathlib.Path(deploy_path, "blog.html").open("r") as f:
+                content = f.read()
+
+            self.assertEqual(content, "<p>0</p><p>1</p><p>2</p>")
+
+    def test_filter_not_matching(self):
+        with TemporaryDirectory() as content_path, TemporaryDirectory() as deploy_path:
+            path = pathlib.Path(content_path, "blog.htm")
+            with path.open("w") as f:
+                f.write(PLAIN_TEMPLATE)
+
+            node = Node(path, Node(path.parent, None, {"content_path": content_path,
+                                                       "deploy_path": deploy_path,
+                                                       "filter": "exhibition.filters.jinja2",
+                                                       "templates": []}))
+            node.render()
+            with pathlib.Path(deploy_path, "blog.htm").open("r") as f:
+                content = f.read()
+
+            self.assertEqual(content, PLAIN_TEMPLATE)
+
+    def test_filter_glob(self):
+        with TemporaryDirectory() as content_path, TemporaryDirectory() as deploy_path:
+            path = pathlib.Path(content_path, "blog.htm")
+            with path.open("w") as f:
+                f.write(PLAIN_TEMPLATE)
+
+            node = Node(path, Node(path.parent, None, {"content_path": content_path,
+                                                       "deploy_path": deploy_path,
+                                                       "filter": "exhibition.filters.jinja2",
+                                                       "filter-glob": "*.htm",
+                                                       "templates": []}))
+            node.render()
+            with pathlib.Path(deploy_path, "blog.htm").open("r") as f:
+                content = f.read()
+
+            self.assertEqual(content, "<p>0</p><p>1</p><p>2</p>")
